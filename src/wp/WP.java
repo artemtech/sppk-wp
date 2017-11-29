054f99f6ce4b352f7ee107fb25627cf82e37c34a
artemtech <sofyanartem@gmail.com>
2017-11-30 06:50:40 +0700

    fix vector S calculation error

--- src/wp/WP.java
+++ src/wp/WP.java
@@ -15,11 +15,30 @@ public class WP {
         Scanner inputs = new Scanner(System.in);
         System.out.print("masukkan jumlah kriteria: ");
         JUMLAH_KRITERIA = inputs.nextInt();
+        // dummy data
+//        JUMLAH_KRITERIA = 4;
         /*
          * [
          * Nama Kriteria : bobot ]
          */
         String daftarKriteria[][] = new String[JUMLAH_KRITERIA][3];
+//        // dummy data:
+//        daftarKriteria[0][0] = "jarak";
+//        daftarKriteria[0][1] = "3";
+//        daftarKriteria[0][2] = "c";
+//        
+//        daftarKriteria[1][0] = "harga";
+//        daftarKriteria[1][1] = "4";
+//        daftarKriteria[1][2] = "c";
+//        
+//        daftarKriteria[2][0] = "kualitas pupuk";
+//        daftarKriteria[2][1] = "5";
+//        daftarKriteria[2][2] = "b";
+//        
+//        daftarKriteria[3][0] = "dokumentasi";
+//        daftarKriteria[3][1] = "2";
+//        daftarKriteria[3][2] = "b";
+        
         for (int i = 0; i < daftarKriteria.length; i++) {
             System.out.printf("Kriteria C%d:\n", i + 1);
             System.out.println("-----------------");
@@ -36,7 +55,26 @@ public class WP {
         //------------------------------------------------------------------
         System.out.print("Masukkan jumlah alternatif: ");
         int JUMLAH_ALTERNATIF = inputs.nextInt();
+//        int JUMLAH_ALTERNATIF = 3;
         String alternatif[][] = new String[JUMLAH_ALTERNATIF][JUMLAH_KRITERIA + 1];
+//        //dummy data:
+//        alternatif[0][0] = "cv. surya bahagia";
+//        alternatif[0][1] = "66";
+//        alternatif[0][2] = "50";
+//        alternatif[0][3] = "4";
+//        alternatif[0][4] = "3";
+//
+//        alternatif[1][0] = "cv. lambang keseangan";
+//        alternatif[1][1] = "30";
+//        alternatif[1][2] = "150";
+//        alternatif[1][3] = "5";
+//        alternatif[1][4] = "5";
+//
+//        alternatif[2][0] = "cv. seger waras";
+//        alternatif[2][1] = "35";
+//        alternatif[2][2] = "70";
+//        alternatif[2][3] = "5";
+//        alternatif[2][4] = "4";
         for (int i = 0; i < alternatif.length; i++) {
             System.out.printf("Alternatif ke%d:\n", i + 1);
             System.out.println("-----------------");
@@ -87,20 +125,22 @@ public class WP {
     }
 
     private static double[] hitungVektorS(String[][] daftarKriteria, String[][] alternatif, double[] normalisasi) {
-        double hasil[] = new double[alternatif.length];
+        double hasil[] = new double[alternatif.length]; //3
         
-        for (int i = 0; i < hasil.length; i++) {
+        for (int i = 0; i < hasil.length; i++) { // 0 - 2
             double hasilSi = 0;
             double hasilPerAlternatif = 1;
-            for (int j = 0; j < alternatif[i].length - 1; j++) {
+//            System.out.println("alternatif ke: "+i);
+            for (int j = 1; j < alternatif[i].length; j++) { // 1 - 4
                 // kalau cost normalisasi negatif (untuk pangkat)
-                if (daftarKriteria[i][2].compareToIgnoreCase("c") == 0) {
-                    hasilSi = Math.pow(Double.parseDouble(alternatif[i][j+1]), -normalisasi[j]);
+                if (daftarKriteria[j-1][2].compareToIgnoreCase("c") == 0) {
+//                    System.out.printf("cost! %d %d\n",i,j);
+                    hasilSi = Math.pow(Double.parseDouble(alternatif[i][j]), -normalisasi[j-1]);
                 }
-                if(daftarKriteria[i][2].compareToIgnoreCase("b") == 0){
-                    hasilSi = Math.pow(Double.parseDouble(alternatif[i][j+1]), normalisasi[j]);
+                else if(daftarKriteria[j-1][2].compareToIgnoreCase("b") == 0){
+//                    System.out.printf("benefit!%d %d\n",i,j);
+                    hasilSi = Math.pow(Double.parseDouble(alternatif[i][j]), normalisasi[j-1]);
                 }
-                // S(i) = c(i)(j) ^ w(i)
 //                System.out.println("Normalisasi j:"+normalisasi[j]);
                 hasilPerAlternatif = hasilPerAlternatif * hasilSi;
             }
